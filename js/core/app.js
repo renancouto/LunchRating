738a12643082f62cb5877f8d33e5f622e8c66fe9
Renan Couto <renancouto@gmail.com>
2013-05-09 17:56:32 -0300

    Login behavior added but not completed

--- js/core/app.js
+++ js/core/app.js
@@ -4,37 +4,56 @@ LunchRating.config = {
 	jsKey: 'EjVkdcArfvUxLcVkHSDL07b3feSIS5XXClrBJt5h'
 };
 
-LunchRating.init = function Init () {
+LunchRating.init = function initF () {
 	Parse.initialize(LunchRating.config.appId, LunchRating.config.jsKey);
 	this.userSetup();
 };
 
 LunchRating.userSetup = function UserSetup () {
-	var apply = function Apply () {
+	var apply = function applyF () {
 		LunchRating.user = Parse.User.current();
 
-		$('#username').html(LunchRating.user.attributes.username);
+		$('#username')
+			.show()
+			.find('.name')
+				.html(LunchRating.user.attributes.username);
+
 		$('#logout').on('click', function(){
 			Parse.User.logOut();
 		});
 	},
 
-	signUp = function signUpP (email, password) {
+	signUp = function signUpF (email, password) {
 		Parse.User.signUp(email, password, { ACL: new Parse.ACL() }, {
 			success: function(user) {
 				console.log(user);
-				// new ManageTodosView();
-				// self.undelegateEvents();
-				// delete self;
 			},
 
 			error: function(user, error) {
-				console.log(user, error);
-				// self.$(".signup-form .error").html(error.message).show();
-				// this.$(".signup-form button").removeAttr("disabled");
+				console.error(user, error);
+			}
+		});
+	},
+
+	logIn = function logInF (email, password) {
+		Parse.User.logIn(email, password, {
+			success: function(user) {
+				console.log(user);
+			},
+
+			error: function(user, error) {
+				console.error(user, error);
 			}
 		});
 	};
+
+	$('#login-form').on('submit', function(e){
+		e.preventDefault();
+		var $this = $(this),
+			$input = $this.find('input');
+
+		logIn($input.filter('[name=email]').val(), $input.filter('[name=password]').val());
+	});
 
 	// Check if user is logged
 	if (Parse.User.current()) {
